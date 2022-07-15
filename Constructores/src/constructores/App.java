2fdec3e12ff08c9aa1044407bc6657908900def7
Alex <ronaldalexandervacca@unicesar.edu.co>
2022-07-14 19:55:54 -0500

    Ejemplo Modificacion

--- Constructores/src/constructores/App.java
+++ Constructores/src/constructores/App.java
@@ -18,6 +18,7 @@ public class App {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        //Metodos
         Metodos.Menu();
         
         String nombre="Alex";
